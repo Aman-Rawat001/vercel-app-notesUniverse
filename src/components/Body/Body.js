93b39e7110ac9df13b5850315c31d4a0686cb933
Aman Rawat <amanrawa2@gmail.com>
2021-06-01 09:50:48 +0530

    uploadnotes working start

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Body.css";
 
 //importing components.
-import SearchNotes from "./SearchNotes/SearchNotes.js";
+// import SearchNotes from "./SearchNotes/SearchNotes.js";
+import UploadNotes from "./UploadNotes/UploadNotes.js";
 
 const Body = () => {
   return (
     <>
-      <SearchNotes />
+      {/* <SearchNotes /> */}
+      <UploadNotes />
     </>
   );
 };
