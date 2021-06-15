f4ca296b2235131620eab5045e5cb445f066717d
Aman Rawat <amanrawa2@gmail.com>
2021-06-15 10:55:47 +0530

    dropdown menu change

--- src/components/Body/SearchNotes/SearchNotes.js
+++ src/components/Body/SearchNotes/SearchNotes.js
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchNotes = () => {
+  const [user, setUser] = useState([]);
+
   return (
     <>
       <div
@@ -43,180 +45,69 @@ const SearchNotes = () => {
             {/* dropdowns */}
             <div className="dropdown_menu row">
               <div className="dropdown col-md-3 col-sm-6 col-6 search_dropdown">
-                <button
-                  className="btn btn-secondary dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton2"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                <select
+                  name="subject"
+                  id="select_sub"
+                  className="form-select select_dropdown"
+                  aria-label="Default select example"
                 >
-                  Institution
-                </button>
-                <ul
-                  className="dropdown-menu dropdown-menu-dark"
-                  aria-labelledby="dropdownMenuButton2"
-                >
-                  <li>
-                    <a className="dropdown-item active" href="#">
-                      Graphic Era University
-                    </a>
-                  </li>
-                </ul>
+                  <option selected>Institution</option>
+                  <option value="1">Graphic era university</option>
+                </select>
               </div>
               <div className="dropdown col-md-3 col-sm-6 col-6 search_dropdown">
-                <button
-                  className="btn btn-secondary dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton2"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                <select
+                  name="subject"
+                  id="select_sub"
+                  className="form-select select_dropdown"
+                  aria-label="Default select example"
                 >
-                  Course
-                </button>
-                <ul
-                  className="dropdown-menu dropdown-menu-dark"
-                  aria-labelledby="dropdownMenuButton2"
-                >
-                  <li>
-                    <a className="dropdown-item active" href="#">
-                      B.Tech c.s.e
-                    </a>
-                  </li>
-                </ul>
+                  <option selected>Course</option>
+                  <option value="1">B.tech c.s.e</option>
+                </select>
               </div>
               <div className="dropdown col-md-3 col-sm-6 col-6 search_dropdown">
-                <button
-                  className="btn btn-secondary dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton2"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                <select
+                  name="semester"
+                  id="select_sem"
+                  className="form-select select_dropdown"
+                  aria-label="Default select example"
                 >
-                  Semester
-                </button>
-                <ul
-                  className="dropdown-menu dropdown-menu-dark"
-                  aria-labelledby="dropdownMenuButton2"
-                >
-                  <li>
-                    <a className="dropdown-item active" href="#">
-                      Semester-1
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-2
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-3
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-4
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-5
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-6
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-7
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Semester-8
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                </ul>
+                  <option selected>Semester</option>
+                  <option value="1">Semester-1</option>
+                  <option value="2">Semester-2</option>
+                  <option value="3">Semester-3</option>
+                  <option value="4">Semester-4</option>
+                  <option value="5">Semester-5</option>
+                  <option value="6">Semester-6</option>
+                  <option value="7">Semester-7</option>
+                  <option value="8">Semester-8</option>
+                </select>
               </div>
               <div className="dropdown col-md-3 col-sm-6 col-6 search_dropdown">
-                <button
-                  className="btn btn-secondary dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton2"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                <select
+                  name="subject"
+                  id="select_sub"
+                  className="form-select select_dropdown"
+                  aria-label="Default select example"
                 >
-                  Subject
-                </button>
-                <ul
-                  className="dropdown-menu dropdown-menu-dark"
-                  aria-labelledby="dropdownMenuButton2"
-                >
-                  <li>
-                    <a className="dropdown-item active" href="#">
-                      Fundamentals of computer programming
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Data Structures
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Discrete mathematics
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Database management system
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Theory of computation
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Java programming
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Artificial intelligence(AI)
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Internet of things(IOT)
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Cyber security
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Compiler Design
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Microprocessors and Micrcontrollers
-                    </a>
-                  </li>
-
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                </ul>
+                  <option selected>Subject</option>
+                  <option value="1">
+                    Fundamentals of computer programming
+                  </option>
+                  <option value="2">Data Structures</option>
+                  <option value="3">Discrete mathematics</option>
+                  <option value="4">Database management system</option>
+                  <option value="5">Theory of computation</option>
+                  <option value="6">Java programming</option>
+                  <option value="7">Artificial intelligence(AI)</option>
+                  <option value="8">Internet of things(IOT)</option>
+                  <option value="9">Cyber security</option>
+                  <option value="10">Compiler Design</option>
+                  <option value="11">
+                    Microprocessors and Micrcontrollers
+                  </option>
+                </select>
               </div>
             </div>
             <hr className="my-5" />
