319fa2459b593c7d90a5d4d33a1dbc6aac00bf67
Aman Rawat <amanrawa2@gmail.com>
2021-06-22 12:35:21 +0530

    emailjs env done

--- src/components/Body/SendEmail/apiKey.js
+++ src/components/Body/SendEmail/apiKey.js
@@ -2,8 +2,8 @@ import { init } from "emailjs-com";
 init("user_V6sFOHTVXZLMnCut5wbJJ");
 
 export default {
-  SERVICE_ID: "service_aflvgq4",
-  USER_ID: "user_V6sFOHTVXZLMnCut5wbJJ",
-  TEMPLATE_ID_CONTACT: "template_zn1gqlo",
-  TEMPLATE_ID_UPLOAD: "template_b86tx89",
+  SERVICE_ID: process.env.REACT_APP_SERVICE_ID,
+  USER_ID: process.env.REACT_APP_USER_ID,
+  TEMPLATE_ID_CONTACT: process.env.REACT_APP_TEMPLATE_ID_CONTACT,
+  TEMPLATE_ID_UPLOAD: process.env.REACT_APP_TEMPLATE_ID_UPLOAD,
 };
