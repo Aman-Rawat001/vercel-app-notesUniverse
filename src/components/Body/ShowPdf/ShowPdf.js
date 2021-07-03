9b7a9d24d418dcbfaa0c4cad63be2793c91d2e00
Aman Rawat <amanrawa2@gmail.com>
2021-07-03 11:51:56 +0530

    getting CORS error

--- src/components/Body/ShowPdf/ShowPdf.js
+++ src/components/Body/ShowPdf/ShowPdf.js
@@ -3,8 +3,15 @@ import { useParams } from "react-router-dom";
 import "./ShowPdf.css";
 import firebase from "../../../firebase";
 
+import { Viewer } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { Worker } from "@react-pdf-viewer/core";
+
 const ShowPdf = () => {
   const [showLink, setShowLink] = useState("");
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   const db = firebase.firestore();
   let { pdfId } = useParams();
@@ -14,7 +21,6 @@ const ShowPdf = () => {
     data.docs.forEach((item) => {
       if (pdfId === item.id) {
         setShowLink(item.data().pdfLink);
-        // console.log(item.data().pdfLink);
       }
     });
   };
@@ -22,33 +28,27 @@ const ShowPdf = () => {
   useEffect(() => {
     searchPdf();
   });
+
   return (
     <div className="mt-5 text-center">
-      {/* <h1 className="pt-5">{pdfId}</h1> */}
-      <div className="holds-the-iframe">
-        <object
-          className="mt-4"
-          data={`${showLink}`}
-          type="application/pdf"
-          width="100%"
-          height="600px"
-        ></object>
-      </div>
-      <div className="showOnSmallScreen my-5 container">
-        <div className="py-5 text-center">
-          <p>
-            <span style={{ fontWeight: "600" }}>Sorry! .</span>
-            Your mobile browser doesn't support online PDF viewer.
-            <span className="text_bold" style={{ fontWeight: "600" }}>
-              {" "}
-              Open in computer or
-            </span>{" "}
-          </p>
-          <a href={`${showLink}`}>Download Notes PDF</a>
-        </div>
-      </div>
+      {/* using react pdf viewer library but it is getting CORS error. */}
+      {
+        <>
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+            <Viewer
+              fileUrl={showLink}
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          </Worker>
+        </>
+      }
     </div>
   );
 };
 
 export default ShowPdf;
+// packages used in pdf-Viewer(don't forgot to uninstall them)
+
+// 1- install pdfjs library | npm install pdfjs-dist@2.6.347
+// 2- install the core package | npm install @react-pdf-viewer/core@2.4.1
+// 3- install pdf viewer default layout | npm install @react-pdf-viewer/default-layout
